d9470761c823a36975df8f97abdf6e3a71d3303d
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-26 13:00:00 +0530

    169. Majority Element

---
+++ hash-table/169. Majority Element.go
@@ -0,0 +1,16 @@
+package hashtable
+
+func majorityElement(nums []int) int {
+	// max := 0
+	m := map[int]int{}
+
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]]++
+	}
+	for i := 0; i < len(nums); i++ {
+		if m[nums[i]] > len(nums)/2 {
+			return nums[i]
+		}
+	}
+	return 0
+}

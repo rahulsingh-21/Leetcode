75c504e46c59da84f02c6ffee897bb8b9d300a96
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-26 12:00:00 +0530

    80. Remove Duplicates from Sorted Array II

---
+++ two-pointers/80. Remove Duplicates from Sorted Array II.go
@@ -0,0 +1,20 @@
+package main
+
+func removeDuplicates2(nums []int) int {
+	ans, c := 0, 0
+	nums[ans] = nums[0]
+	ans++
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			c = 0
+		} else {
+			c++
+		}
+		if c >= 2 {
+			continue
+		}
+		nums[ans] = nums[i]
+		ans++
+	}
+	return ans
+}

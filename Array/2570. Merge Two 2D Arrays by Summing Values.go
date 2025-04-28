7f58ebe2c8849dc9a0140cba93dcd677e238992e
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-28 14:00:00 +0530

    2570. Merge Two 2D Arrays by Summing Values

---
+++ Array/2570. Merge Two 2D Arrays by Summing Values.go
@@ -0,0 +1,27 @@
+package array
+
+import "sort"
+
+func mergeArrays(nums1 [][]int, nums2 [][]int) [][]int {
+	m := map[int]int{}
+
+	for i := 0; i < len(nums1); i++ {
+		m[nums1[i][0]] += nums1[i][1]
+	}
+	for i := 0; i < len(nums2); i++ {
+		m[nums2[i][0]] += nums2[i][1]
+	}
+
+	var n []int
+	for key, _ := range m {
+		n = append(n, key)
+	}
+	sort.Ints(n)
+	var ans [][]int
+	for i := 0; i < len(n); i++ {
+		var temp []int
+		temp = append(temp, n[i], m[n[i]])
+		ans = append(ans, temp)
+	}
+	return ans
+}

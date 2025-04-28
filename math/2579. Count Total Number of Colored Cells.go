7ad2fe25b18ddd1f2960f5014b978a61111103b3
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-28 16:00:00 +0530

    2579. Count Total Number of Colored Cells

---
+++ math/2579. Count Total Number of Colored Cells.go
@@ -0,0 +1,22 @@
+package math
+
+func coloredCells(n int) int64 {
+	var ans int64
+	if n == 1 {
+		return 1
+	}
+	ans += int64(n) + int64(n-1)
+	temp := ans
+	for temp > 0 {
+		x := temp - 2
+		if x < 0 {
+			break
+		}
+		//fmt.Println(ans, x)
+		ans = ans + (2 * x)
+		temp = temp - 2
+		//fmt.Println(ans, x)
+	}
+	//fmt.Println(ans)
+	return ans
+}

d4e6c796d8d4f3e64be86937786f48b18fce6176
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-28 12:00:00 +0530

    66. Plus One

---
+++ math/66. Plus One.go
@@ -0,0 +1,15 @@
+package math
+
+func plusOne(digits []int) []int {
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] += 1
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	digits = append([]int{1}, digits...)
+	return digits
+}

dfe3c4310c19947acc60c9904781f8ca16d183e5
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-04-30 10:00:00 +0530

    2965. Find Missing and Repeated Values

---
+++ hash-table/2965. Find Missing and Repeated Values.go
@@ -0,0 +1,21 @@
+package hashtable
+
+func findMissingAndRepeatedValues(grid [][]int) []int {
+	m := make([]int, len(grid)*len(grid)+1)
+	ans := make([]int, 2)
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid); j++ {
+			m[grid[i][j]]++
+		}
+	}
+
+	//fmt.Println(m)
+	for i := 1; i < len(m); i++ {
+		if m[i] == 2 {
+			ans[0] = i
+		} else if m[i] == 0 {
+			ans[1] = i
+		}
+	}
+	return ans
+}

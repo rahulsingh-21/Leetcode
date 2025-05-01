e4cd72f7ddce8bc37fa91c7361f93d14ae60a5b8
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-01 10:00:00 +0530

    240. Search a 2D Matrix II

---
+++ matrix/240. Search a 2D Matrix II.go
@@ -0,0 +1,19 @@
+package matrix
+
+func searchMatrix(matrix [][]int, target int) bool {
+	n := len(matrix)
+	m := len(matrix[0])
+
+	i, j := 0, m-1
+
+	for i < n && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			j -= 1
+		} else {
+			i += 1
+		}
+	}
+	return false
+}

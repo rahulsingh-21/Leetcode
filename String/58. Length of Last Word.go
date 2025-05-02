c2823fe5b6a8a4e2c46ddc5da96d6413711d4d90
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-02 11:00:00 +0530

    58. Length of Last Word

---
+++ String/58. Length of Last Word.go
@@ -0,0 +1,13 @@
+package string
+
+import "strings"
+
+func lengthOfLastWord(s string) int {
+	arr := strings.Split(s, " ")
+	for i := len(arr) - 1; i >= 0; i-- {
+		if len(arr[i]) > 0 {
+			return len(arr[i])
+		}
+	}
+	return 0
+}

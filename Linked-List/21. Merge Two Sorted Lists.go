ae6412e07c7cfb165b3f301be797aef6e792bf83
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-02 12:00:00 +0530

    21. Merge Two Sorted Lists

---
+++ Linked-List/21. Merge Two Sorted Lists.go
@@ -0,0 +1,25 @@
+package linkedlist
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	node := &ListNode{}
+	dummy := node
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			dummy.Next = list1
+			list1 = list1.Next
+		} else {
+			dummy.Next = list2
+			list2 = list2.Next
+		}
+		dummy = dummy.Next
+	}
+
+	if list1 != nil {
+		dummy.Next = list1
+		//list1 =list1.Next
+	} else {
+		dummy.Next = list2
+		//list2 = list2.Next
+	}
+	return node.Next
+}

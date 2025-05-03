784f927e54cef8e127d18fc40fe60f915cbb3ecd
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-03 14:00:00 +0530

    2149. Rearrange Array Elements by Sign

---
+++ two-pointers/2149. Rearrange Array Elements by Sign.go
@@ -0,0 +1,19 @@
+package twopointers
+
+func rearrangeArray(nums []int) []int {
+	ans := make([]int, len(nums))
+
+	i := 0
+	j := 1
+
+	for k := 0; k < len(nums); k++ {
+		if nums[k] > 0 {
+			ans[i] = nums[k]
+			i += 2
+		} else {
+			ans[j] = nums[k]
+			j += 2
+		}
+	}
+	return ans
+}

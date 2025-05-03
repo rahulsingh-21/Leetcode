e19d722f8d4ca307f6f95393f6092e484e600788
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-03 14:00:00 +0530

    1903. Largest Odd Number in String

---
+++ math/1903. Largest Odd Number in String.go
@@ -0,0 +1,11 @@
+package math
+
+func largestOddNumber(num string) string {
+	// ans := ""
+	index := len(num) - 1
+	for index = len(num) - 1; index >= 0 && num[index]%2 == 0; {
+		index--
+	}
+
+	return num[:index+1]
+}

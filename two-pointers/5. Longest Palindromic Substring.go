d06d84835b4cb9193c5251cf479b23396c85adf3
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-04 10:00:00 +0530

    5. Longest Palindromic Substring

---
+++ two-pointers/5. Longest Palindromic Substring.go
@@ -0,0 +1,25 @@
+package twopointers
+
+func longestPalindrome(s string) string {
+	ans := ""
+	for i := 0; i < len(s); i++ {
+		odd := check(i, i, s)
+		if len(odd) > len(ans) {
+			ans = odd
+		}
+		even := check(i, i+1, s)
+		if len(even) > len(ans) {
+			ans = even
+		}
+	}
+	return ans
+}
+
+func check(l, r int, s string) string {
+
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return s[l+1 : r]
+}

a3def8794aa0850db3b649fa25a32174592d57b9
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-07 10:00:00 +0530

    56. Merge Intervals

---
+++ 56. Merge Intervals.go
@@ -0,0 +1,19 @@
+package main
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	var ans [][]int
+	for _, val := range intervals {
+		if len(ans) == 0 || ans[len(ans)-1][1] < val[0] {
+			ans = append(ans, val)
+		} else {
+			ans[len(ans)-1][1] = max(ans[len(ans)-1][1], val[1])
+		}
+	}
+	return ans
+}

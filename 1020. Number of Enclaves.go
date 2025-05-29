6d67329064379fbb086ef45c241cd6190f79c34e
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-05-29 10:00:00 +0530

    1020. Number of Enclaves

---
+++ 1020. Number of Enclaves.go
@@ -0,0 +1,41 @@
+package main
+
+func numEnclaves(board [][]int) int {
+	n := len(board)
+	m := len(board[0])
+	q := [][]int{}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if board[i][j] == 1 && (i == 0 || j == 0 || i == n-1 || j == m-1) {
+				board[i][j] = 'R'
+				q = append(q, []int{i, j})
+			}
+		}
+	}
+
+	for len(q) > 0 {
+		for _, node := range q {
+			for _, dir := range directions {
+				i := node[0] + dir[0]
+				j := node[1] + dir[1]
+				if i >= 0 && i < n && j >= 0 && j < m && board[i][j] == 1 {
+					board[i][j] = 'R'
+					q = append(q, []int{i, j})
+				}
+			}
+			q = q[1:]
+		}
+	}
+
+	count := 0
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if board[i][j] == 1 {
+				count++
+			}
+		}
+	}
+	return count
+}

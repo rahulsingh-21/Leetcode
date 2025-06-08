ea28f3356ba281d6fa9a82e147d07752b18fb9b0
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-06-08 10:00:00 +0530

    88. Merge Sorted Array

---
+++ 88. Merge Sorted Array.go
@@ -0,0 +1,19 @@
+package main
+
+func mergeII(nums1 []int, m int, nums2 []int, n int) {
+	p1 := m - 1
+	p2 := n - 1
+
+	for i := m + n - 1; i >= 0; i-- {
+		if p2 < 0 {
+			break
+		}
+		if p1 >= 0 && nums1[p1] > nums2[p2] {
+			nums1[i] = nums1[p1]
+			p1--
+		} else {
+			nums1[i] = nums2[p2]
+			p2--
+		}
+	}
+}

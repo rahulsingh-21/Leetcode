18d830c0cc0b737e465757280b50eeaac7054f0e
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-06-11 10:00:00 +0530

    71. Simplify Path

---
+++ 71. Simplify Path.go
@@ -0,0 +1,24 @@
+package main
+
+import "strings"
+
+func simplifyPath(path string) string {
+	stack := []string{}
+
+	paths := strings.Split(path, "/")
+
+	for _, path := range paths {
+		if path == "" || path == "." {
+			continue
+		}
+		if path == ".." {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+			continue
+		}
+		stack = append(stack, path)
+	}
+
+	return "/" + strings.Join(stack, "/")
+}

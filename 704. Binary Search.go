7f6e36dadea1df5955381cf09712946c09817cae
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-06-14 10:00:00 +0530

    704. Binary Search

---
+++ 704. Binary Search.go
@@ -0,0 +1,18 @@
+package main
+
+func searchII(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		}
+	}
+	return -1
+}

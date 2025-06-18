ad26868ad8d62b3225e9afb79c0c289bc73e094b
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-06-18 10:00:00 +0530

    215. Kth Largest Element in an Array

---
+++ 215. Kth Largest Element in an Array/215. Kth Largest Element in an Array.go
@@ -0,0 +1,33 @@
+package twoonefive
+
+import "container/heap"
+
+func findKthLargest(nums []int, k int) int {
+
+	h := &MinHeap{}
+	heap.Init(h)
+
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return (*h)[0]
+}
+
+type MinHeap []int
+
+func (m MinHeap) Len() int           { return len(m) }
+func (m MinHeap) Less(i, j int) bool { return m[i] < m[j] }
+func (m MinHeap) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m *MinHeap) Push(v interface{}) {
+	*m = append(*m, v.(int))
+}
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}

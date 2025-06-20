d8b3f954a0e8b9b7ce1fc5cc3605c7b5c9845e6f
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-06-20 10:00:00 +0530

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.go
@@ -0,0 +1,14 @@
+package main
+
+func firstUniqChar(s string) int {
+	m := make(map[rune]int)
+	for _, char := range s {
+		m[char]++
+	}
+	for i, char := range s {
+		if m[char] == 1 {
+			return i
+		}
+	}
+	return -1
+}

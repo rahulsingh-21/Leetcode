49811b789e77dbd24ed8aef228738b2441f40f1c
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-07-18 10:00:00 +0530

    72. Edit Distance

---
+++ 72. Edit Distance.go
@@ -0,0 +1,31 @@
+package main
+
+func minDistanceII(word1 string, word2 string) int {
+	n := len(word1)
+	m := len(word2)
+
+	dp := make([][]int, n+1)
+
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 0; i <= n; i++ {
+		dp[i][0] = i
+	}
+
+	for j := 0; j <= m; j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])) + 1
+			}
+		}
+	}
+	return dp[n][m]
+}

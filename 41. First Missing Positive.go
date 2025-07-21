85605369951fc71d8de2a492fee085f39434d562
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-07-21 10:00:00 +0530

    41. First Missing Positive

---
+++ 41. First Missing Positive.go
@@ -0,0 +1,21 @@
+package main
+
+func firstMissingPositive(nums []int) int {
+
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+			i--
+		}
+
+	}
+
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+
+	return n + 1
+}

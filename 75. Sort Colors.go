329d0ffc35f2b5fa97207b112d78dd83b0b5a7af
Rahul Singh <rahulsinghsgcl@gmail.com>
2025-07-21 10:00:00 +0530

    sort colors optimized

--- 75. Sort Colors.go
+++ 75. Sort Colors.go
@@ -1,16 +1,18 @@
 package main
 
 func sortColors(nums []int) {
-	c0, c2 := 0, len(nums)-1
-	for i := 0; i < len(nums); i++ {
-		for (nums[i] == 0 || nums[i] == 2) && i >= c0 && c2 >= i {
-			if nums[i] == 0 {
-				nums[i], nums[c0] = nums[c0], nums[i]
-				c0++
-			} else if nums[i] == 2 {
-				nums[i], nums[c2] = nums[c2], nums[i]
-				c2--
-			}
+	p0, p2, curr := 0, len(nums)-1, 0
+
+	for curr <= p2 {
+		if nums[curr] == 0 {
+			nums[curr], nums[p0] = nums[p0], nums[curr]
+			curr++
+			p0++
+		} else if nums[curr] == 2 {
+			nums[curr], nums[p2] = nums[p2], nums[curr]
+			p2--
+		} else {
+			curr++
 		}
 	}
 }
